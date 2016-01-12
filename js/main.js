239a4c04091ae0b1cb16e180d7248c3cb255e477
CuriousLearner <sanyam.khurana01@gmail.com>
2016-01-12 14:02:22 +0000

    Explara tickets fetching JS code added

--- js/main.js
+++ js/main.js
@@ -5098,6 +5098,20 @@ $(document).ready(function(){
     }
   });
 
+  var tickets_url = "http://127.0.0.1:5000/api/tickets-left/";
+  var tickets_left = "No fetched";
+  $.ajax({
+    url: tickets_url
+  }).done(function(data) {
+    console.log("Tickets fetched");
+    tickets_left = data.tickets_left;
+    console.log(tickets_left);
+    $('#tickets-left').html(tickets_left);
+  }).fail(function(data) {
+    console.log("Failed to fetch tickets");
+    console.log(data);
+  });
+
   //Modal for Forms
   function hideModal() {
     $('.modal-wrap').fadeOut(300);
