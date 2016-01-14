0f9fc61eeed6e819fcf866d8932a83ce166135c7
kushagra43 <kushagra43@gmail.com>
2016-01-15 03:12:42 +0530

    added travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # conference
 Python Local Conference, hosted by Python Delhi User Group (Pydelhi).
+
+[![Build Status](https://travis-ci.org/Kushagra343/conference.svg?branch=static_template)](https://travis-ci.org/Kushagra343/conference)
--- gh-pages-deploy.sh
+++ gh-pages-deploy.sh
@@ -4,4 +4,4 @@ git config user.name "Travis-CI"
 git config user.email "travis@email.com"
 git add .
 git commit -m "Deployed to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/Kushagra343/conf123" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/Kushagra343/conference" master:gh-pages > /dev/null 2>&1
