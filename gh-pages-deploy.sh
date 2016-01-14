b20be72ba9025aee08ef5f827882bb0dd9fdcaf3
kushagra43 <kushagra43@gmail.com>
2016-01-15 03:01:38 +0530

    created jinja template and automated it

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,7 @@
+mkdir build; cp index.html build/;cp -r vendor build/;cp -r pamphlet build/;cp -r js build/;cp -r img build/;cp -r data build/;cp -r css build/; cd build
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@email.com"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/Kushagra343/conf123" master:gh-pages > /dev/null 2>&1
