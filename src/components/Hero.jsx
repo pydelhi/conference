7e39c1fac34de5e5e97f0b6596ce4aa608dc1edd
codecShivam <codecshivam@gmail.com>
2023-05-30 10:28:20 +0530

    improved hero component on homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import delhi from "../assets/herobg.png"
+import { useLottie } from "lottie-react";
+import "@lottiefiles/lottie-player";
 // import fitt from "../assets/fitt.png"
 import './Hero.css';
+import animationData from "../lotties/LottieAnimation.json"
+
 
 const Hero = () => {
+
+    const options = {
+        animationData: animationData,
+        loop: true
+    };
+
+    const { View } = useLottie(options);
+
     return (
         <div>
-            <img src={delhi} className="w-[100%]  sm:h-[36rem] h-[19rem] " alt="" />
+            <img src={delhi} className="w-[100%]   sm:h-[36rem] h-[19rem] " alt="" />
 
             <section className=" ">
                 <div className="flex">
-                    <div className="mr-auto pt-6  sm:pt-10 sm:mx-[3.2REM] mx-4 place-self-center lg:col-span-7">
-                        <h1 className="text-2xl  font-extrabold tracking-tight leading-none sm:text-4xl md:text-5xl ">PY<spand>DELHI</spand> CONFERENCE</h1>
-                        <p className="text-sm sm:text-md tracking-tight sm:tracking-wider">CONFERENCE ON PYTHON PROGRAMMING LANGUAGE</p>
+                    <div className="mr-auto pt-6  sm:pt-12 sm:mx-[3.2REM] mx-4 place-self-center lg:col-span-7">
+                        <h1 className="text-2xl  font-extrabold tracking-tight leading-none sm:text-5xl md:text-6xl ">PY<spand>DELHI</spand> CONFERENCE</h1>
+                        <p className="text-sm sm:text-lg tracking-tight sm:tracking-wider">CONFERENCE ON PYTHON PROGRAMMING LANGUAGE</p>
                     </div>
                     {/* <div class="hidden lg:mt-0 lg:col-span-5 lg:flex lg:justify-end">
                         <img src={fitt} className="w-20 " alt="" />
                     </div> */}
-                    <div className="animation-wrapper sm:block hidden z-10 absolute sm:left-[4rem] sm:-mt-4">
-                        <div className="ml-[1.4rem] circle"></div>
-                        <div className="ml-[2.4rem] circle"></div>
-                        <div className="ml-[3.4rem] circle"></div>
-                        <div className="ml-[4.4rem] circle"></div>
-                        <div className="ml-[5.4rem] circle"></div>
-                        <div className="ml-[6.4rem] circle"></div>
-                        <div className="ml-[7.4rem] circle"></div>
-                        <div className="ml-[8.4rem] circle"></div>
+                    <div className="w-44">
+                        {View}
                     </div>
                 </div>
-            </section>
-        </div>
+            </section >
+        </div >
     )
 }
 
